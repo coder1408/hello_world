16759479639f0d2d88e984516eb3803d27edfad4
Ishan <ishanyateenjoshi@gmail.com>
2022-12-06 22:18:46 +0530

    First release of Hello World!

---
+++ Basics.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void
+main ()
+{
+  int a = 10, b = 20;
+  int c;
+  int d;
+  int e;
+  int f;
+  c = a + b;
+  d = a * b;
+  e = b / a; 
+  f = b - a;
+  printf ("\tResult is=%d", c);
+  printf ("\tResult is=%d", d);
+  printf ("\tResult is=%d", e);
+  printf ("\tResult is=%d", f);
+}
